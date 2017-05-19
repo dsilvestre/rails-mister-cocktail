1bb08820a5a62a7429d285516542adeedbbfce9c
Daniel Silvestre <dsilvestre7@gmail.com>
2017-05-19 16:52:28 +0100

    Users can now upload cocktail photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def cocktail_image_path(cocktail)
+    if cocktail.photo?
+      cl_image_path cocktail.photo, height: 300, width: 400, crop: :fit
+    else
+      ""
+    end
+  end
 end
